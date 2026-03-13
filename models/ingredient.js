58c70614c79550a31e3500835489dc106e49ee61
thara-messeroux <tharamesseroux@gmail.com>
2026-03-12 21:03:43 -0400

    feat: add Ingredient model

---
+++ models/ingredient.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const ingredientSchema = new mongoose.Schema({
+    name: String
+});
+
+const Ingredient = mongoose.model("Ingredient", ingredientSchema);
+
+module.exports = Ingredient;
